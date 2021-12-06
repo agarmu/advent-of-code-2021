890f07aeda9db4abdac318b404b5c05fd551c7d3
Nathanael Beisiegel <pknb.dev@gmail.com>
2021-12-05 20:23:13 -0500

    Day 4: Implemented part 1 and 2

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 Solutions to the [Advent of Code 2021](https://adventofcode.com/2021), with the goal of getting stronger at Rust.
 
-![](https://img.shields.io/badge/day%20📅-3-blue)
-![](https://img.shields.io/badge/days%20completed-3-green)
-![](https://img.shields.io/badge/stars%20⭐-6-yellow)
+![](https://img.shields.io/badge/day%20📅-4-blue)
+![](https://img.shields.io/badge/days%20completed-4-green)
+![](https://img.shields.io/badge/stars%20⭐-8-yellow)
 
 ## Running solutions
 
--- day4/src/main.rs
+++ day4/src/main.rs
@@ -4,19 +4,22 @@ use std::{
     str::FromStr,
 };
 
-#[derive(Debug, Default)]
-struct BingoCell {
+static INPUT_FILE: &'static str = include_str!("input.txt");
+const BOARD_SIZE: usize = 5;
+
+#[derive(Debug, Default, Clone, Copy)]
+struct BoardCell {
     number: u8,
     called: bool,
 }
 
 #[derive(Default)]
 struct BingoBoard {
-    board: [[BingoCell; 5]; 5],
+    board: [[BoardCell; BOARD_SIZE]; BOARD_SIZE],
     bingo: bool,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct Position {
     row: usize,
     column: usize,
@@ -29,7 +32,7 @@ impl fmt::Display for BingoBoard {
                 if cell.called {
                     write!(f, "[{:>2}] ", cell.number).unwrap();
                 } else {
-                    write!(f, "{:>4} ", cell.number).unwrap();
+                    write!(f, " {:>2}  ", cell.number).unwrap();
                 }
             }
 
@@ -47,7 +50,7 @@ impl fmt::Debug for BingoBoard {
 }
 
 impl Index<Position> for BingoBoard {
-    type Output = BingoCell;
+    type Output = BoardCell;
     fn index(&self, index: Position) -> &Self::Output {
         &self.board[index.row][index.column]
     }
@@ -63,27 +66,214 @@ impl FromStr for BingoBoard {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let board = Self {
+        let mut board: [[BoardCell; BOARD_SIZE]; BOARD_SIZE] = [[BoardCell {
             ..Default::default()
-        };
+        }; BOARD_SIZE]; BOARD_SIZE];
 
-        return Ok(board);
+        for (row, row_str) in s.split("\n").enumerate() {
+            for (column, cell_str) in row_str.split_ascii_whitespace().enumerate() {
+                board[row][column].number = cell_str.parse().unwrap();
+            }
+        }
+
+        Ok(BingoBoard {
+            board,
+            ..Default::default()
+        })
     }
+}
+
+#[allow(dead_code)]
+struct WinningConditions {
+    number: u8,
+    position: Position,
+    vertical: bool,
+    horizontal: bool,
+    ascending: bool,
+    descending: bool,
 }
 
 impl BingoBoard {
-    fn draw(&mut self, number: u8) -> bool {
-        // iterate over numbers
-        // self.board
-        false
+    fn check_win(&self, from: Position) -> Option<WinningConditions> {
+        let vertical = (0..BOARD_SIZE).all(|r| self[Position { row: r, ..from }].called);
+        let horizontal = (0..BOARD_SIZE).all(|c| self[Position { column: c, ..from }].called);
+
+        // Diagonals don't count...
+        let descending = false;
+        let ascending = false;
+
+        // let descending = from.column == from.row
+        //     && (0..BOARD_SIZE).all(|i| self[Position { column: i, row: i }].called);
+
+        // let ascending = (from.row + from.column) == (BOARD_SIZE - 1)
+        //     && (0..BOARD_SIZE).all(|i| {
+        //         self[Position {
+        //             column: i,
+        //             row: BOARD_SIZE - 1 - i,
+        //         }]
+        //         .called
+        //     });
+
+        if vertical || horizontal || descending || ascending {
+            return Some(WinningConditions {
+                number: self[from].number,
+                position: from,
+                vertical,
+                horizontal,
+                descending,
+                ascending,
+            });
+        }
+
+        None
+    }
+
+    fn draw(&mut self, number: u8) -> Option<WinningConditions> {
+        let mut position: Option<Position> = None;
+
+        for row in 0..BOARD_SIZE {
+            for column in 0..BOARD_SIZE {
+                if self.board[row][column].number == number {
+                    position = Some(Position { row, column });
+                }
+            }
+        }
+
+        match position {
+            Some(position) => {
+                self[position].called = true;
+                self.check_win(position)
+            }
+            None => None,
+        }
+    }
+
+    fn sum_unmarked(&self) -> u64 {
+        let mut sum: u64 = 0;
+
+        for row in 0..BOARD_SIZE {
+            for column in 0..BOARD_SIZE {
+                let cell = self.board[row][column];
+
+                if !cell.called {
+                    sum += cell.number as u64;
+                }
+            }
+        }
+
+        sum
     }
 }
 
-fn main() {
-    let mut board = BingoBoard {
-        ..Default::default()
-    };
+fn get_draws(input: &str) -> Vec<u8> {
+    return input
+        .split("\n")
+        .nth(0)
+        .unwrap()
+        .split(",")
+        .map(|draw| draw.parse().unwrap())
+        .collect();
+}
 
-    println!("{}", board);
-    dbg!(board);
+fn get_boards(input: &str) -> Vec<BingoBoard> {
+    let boards_string = input.split("\n").skip(2).collect::<Vec<&str>>().join("\n");
+
+    return boards_string
+        .split("\n\n")
+        .map(|board| BingoBoard::from_str(board).unwrap())
+        .collect::<Vec<BingoBoard>>();
+}
+
+fn part1(input: &str) -> u64 {
+    let draws = get_draws(input);
+    let mut boards = get_boards(input);
+
+    for draw in &draws {
+        for (board_number, board) in boards.iter_mut().enumerate() {
+            match board.draw(*draw) {
+                Some(_winning_conditions) => {
+                    println!("[Part 1]\n-------------------------------------");
+                    println!(
+                        "The first winning board is {}, with the call of {}. Final board:\n{}",
+                        board_number, draw, board
+                    );
+
+                    let sum = board.sum_unmarked();
+                    let score = sum * *draw as u64;
+
+                    println!("Sum of unmarked numbers: {}; Final score: {}", sum, score);
+                    return score;
+                }
+                _ => (),
+            }
+        }
+    }
+
+    println!("No winners, looks like we didn't handle the input correctly.");
+    dbg!(draws, boards);
+    return 0;
+}
+
+fn part2(input: &str) -> u64 {
+    let draws = get_draws(input);
+    let mut boards = get_boards(input);
+    let final_board_count = boards.len();
+    let mut winning_boards = 0;
+
+    for draw in &draws {
+        for (board_number, board) in boards.iter_mut().enumerate() {
+            if board.bingo {
+                continue;
+            }
+
+            match board.draw(*draw) {
+                Some(_winning_conditions) => {
+                    board.bingo = true;
+                    winning_boards += 1;
+
+                    // last board
+                    if winning_boards == final_board_count {
+                        println!("[Part 2]\n-------------------------------------");
+                        println!(
+                            "The last winning board is {}, with the call of {}. Final board:\n{}",
+                            board_number, draw, board
+                        );
+
+                        let sum = board.sum_unmarked();
+                        let score = sum * *draw as u64;
+
+                        println!("Sum of unmarked numbers: {}; Final score: {}", sum, score);
+                        return score;
+                    }
+                }
+                _ => (),
+            }
+        }
+    }
+
+    println!("No winners, looks like we didn't handle the input correctly.");
+    dbg!(draws, boards);
+    return 0;
+}
+
+fn main() {
+    part1(INPUT_FILE);
+    part2(INPUT_FILE);
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{part1, part2};
+
+    #[test]
+    fn part_1_test_input() {
+        let test_input = include_str!("test.txt");
+        assert_eq!(part1(test_input), 4512);
+    }
+
+    #[test]
+    fn part_2_test_input() {
+        let test_input = include_str!("test.txt");
+        assert_eq!(part2(test_input), 1924);
+    }
 }
